79aab0e57c298cc5c34c26d1b388f811ffe30bab
Carlo Holly <carlo.holly@rwth-aachen.de>
2016-08-25 22:45:26 +0200

    Changes in setup.py.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from distutils.extension import Extension
 
-setup(name="PackageName", ext_modules=[Extension("pymagma",
-                                                 ["pycusp.cpp"],
-                                                 include_dirs = ["src", boost_inc_dir],
-                                                 library_dirs = ["/", boost_lib_dir],
-                                                 libraries = ["boost_python"],
-                                                 extra_compile_args = ["-g"])])
+setup(name='pymagma',
+      version='0.0.1',
+      description='A Python interface to the MAGMA libraries',
+      author='Carlo Holly',
+      author_email='carlo.holly@rwth-aachen.de',
+      url='https://github.com/carloholly/pymagma',
+      packages=['pymagma']
+     )
